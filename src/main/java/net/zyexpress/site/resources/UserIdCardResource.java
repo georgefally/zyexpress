daf2e4e7769f455b60008b8469a3cb5bbae1cdf7
George Fally <george.fally@outlook.com>
2016-02-26 00:06:25 +0900

    add zip export interface

--- src/main/java/net/zyexpress/site/resources/UserIdCardResource.java
+++ src/main/java/net/zyexpress/site/resources/UserIdCardResource.java
@@ -1,15 +1,24 @@
 package net.zyexpress.site.resources;
 
 import com.codahale.metrics.annotation.Timed;
+import com.google.common.io.ByteStreams;
 import net.zyexpress.site.api.UserIdCard;
 import net.zyexpress.site.dao.UserIdCardDAO;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.StreamingOutput;
+import java.io.FileInputStream;
+import java.io.InputStream;
 import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 @Path("/userIdCard")
 @Produces(MediaType.APPLICATION_JSON)
@@ -25,8 +34,7 @@ public class UserIdCardResource {
     @Timed
     @Path("/all")
     public List<UserIdCard> getAllUser() {
-        List a = userIdCardDAO.getAll();
-        return a;
+        return userIdCardDAO.getAll();
     }
 
     @GET
@@ -41,13 +49,21 @@ public class UserIdCardResource {
 
     }
 
-   /* @GET
+    @GET
     @Timed
     @Path("/export")
-    public void exportFile(@Context HttpServletRequest request, @Context HttpServletResponse response,
-                           @QueryParam("fileName")String fileName, @QueryParam("objectName")String objectName,
-                           @QueryParam("docbelong")String docbelong) throws UnsupportedEncodingException {
-        //获得请求文件名
-        System.out.println(fileName);
-    }*/
+    @Produces("application/zip")
+    public StreamingOutput downloadFile(@Context HttpServletRequest request, @Context HttpServletResponse response,
+                                 @QueryParam("ids") List<String> userIds) {
+        return output -> {
+            try (InputStream inputStream1 = new FileInputStream("/var/tmp/image1.png");
+                 InputStream inputStream2 = new FileInputStream("/var/tmp/image2.png");
+                 ZipOutputStream zipOutputStream = new ZipOutputStream(output)) {
+                zipOutputStream.putNextEntry(new ZipEntry("image1.png"));
+                ByteStreams.copy(inputStream1, zipOutputStream);
+                zipOutputStream.putNextEntry(new ZipEntry("image2.png"));
+                ByteStreams.copy(inputStream2, zipOutputStream);
+            }
+        };
+    }
 }
